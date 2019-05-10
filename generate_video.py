f16a9eb8c0c8696e04b5ca845adb37a00d378c30
Carlos Ruiz <carlos.ruiz.dominguez@west.cmu.edu>
2019-05-10 12:28:34 -0700

    WIP ground truth annotation tool

--- generate_video.py
+++ generate_video.py
@@ -57,6 +57,7 @@ def generate_multicam_video(experiment_base_folder, video_out_filename=None, t_s
         f_hdf5.attrs['t_start'] = str(t_cam[0]).encode('utf8')
         f_hdf5.attrs['t_end'] = str(t_cam[-1]).encode('utf8')
         f_hdf5.attrs['fps'] = video_fps
+        f_hdf5.create_dataset('frame_nums', data=frame_nums)
 
     # Generate video
     for n,t in enumerate(t_cam):
@@ -95,7 +96,7 @@ def generate_multicam_video(experiment_base_folder, video_out_filename=None, t_s
     return video_out_filename
 
 
-def generate_video(experiment_base_folder, camera_id=3, weight_id=5309446, do_tare=False, t_lims=5, t_start=0, t_end=-1, weight_plot_scale=0.3, video_fps=25):
+def generate_video(experiment_base_folder, camera_id=3, weight_id=5309446, do_tare=False, t_lims=5, t_start=0, t_end=-1, weight_plot_scale=0.3, video_fps=25, visualize=True, save_video=False, out_scale=0.5):
     multiple_cams = (camera_id < 0)
     if multiple_cams:
         weight_plot_scale = 1  # Overwrite setting, weight plots will be hstacked (same height)
@@ -136,6 +137,7 @@ def generate_video(experiment_base_folder, camera_id=3, weight_id=5309446, do_ta
         ax[i,0].set_title('Shelf {}'.format(shelf_i+1) if multiple_weights else 'Load cell #{}'.format(weight_id))
         ax[i,0].set_ylabel('Weight (g)')
         format_axis_as_timedelta(ax[i,0].xaxis)
+    curr_t_line = ax[-1,0].axvline(0, linestyle='--', color='black', linewidth=1)
     # Render the figure once to get its dimensions
     fig.canvas.draw()
     weight_img = plt_fig_to_cv2_img(fig)
@@ -146,17 +148,30 @@ def generate_video(experiment_base_folder, camera_id=3, weight_id=5309446, do_ta
         rgb_data = np.zeros((rgb_data.shape[0], rgb_data.shape[1]+weight_fig_dimensions[1], 3), dtype=np.uint8)
 
     # Set up video file
-    video_filename = 'AIM3S_experiment.mp4'
-    video_out = cv2.VideoWriter(video_filename, cv2.VideoWriter_fourcc(*'avc1'), video_fps, (video_in_width, video_in_height))
+    if save_video:
+        video_filename = 'AIM3S_experiment.mp4'
+        video_out = cv2.VideoWriter(video_filename, cv2.VideoWriter_fourcc(*'avc1'), video_fps, (video_in_width, video_in_height))
 
-    for n,t in enumerate(t_cam):
-        ok = video_in.read(rgb_data[:,:video_in_width,:])
-        assert ok, "Couldn't read frame {}!".format(n)
+    TIME_INCREMENT = timedelta(seconds=0.1)  # How much to shift the time offset cameras-weights from keyboard input (arrow keys)
+    FRAME_INCREMENT = 10  # How many frames to skip forward/backward on keyboard input (ASDW)
+    LEFT_RIGHT_MULTIPLIER = 10  # How much larger the skip is when using left-right (A-D) vs up-down (or W-S)
+    n = -1  # Frame number
+    curr_t = 0
+    is_paused = False
+    do_skip_frames = False
+    while n < len(t_cam):
+        if not is_paused or do_skip_frames:
+            n += 1
+            ok = video_in.read(rgb_data[:,:video_in_width,:])
+            assert ok, "Couldn't read frame {}!".format(n)
+
+        # Update current time
+        curr_t = (t_cam[n]-weight_to_cam_t_offset).total_seconds()
+        if (t_start > 0 and curr_t < t_start) or (t_end > 0 and curr_t > t_end): continue
+        ax[-1,0].set_xlim(curr_t-t_lims, curr_t+t_lims)
 
         # Update weight plot and convert to image
-        curr_t = (t-weight_to_cam_t_offset).total_seconds()
-        if curr_t < t_start or (t_end > 0 and curr_t > t_end): continue
-        ax[-1,0].set_xlim(curr_t-t_lims, curr_t+t_lims)
+        curr_t_line.set_xdata(curr_t)
         fig.canvas.draw()
         weight_img = plt_fig_to_cv2_img(fig)
 
@@ -167,19 +182,49 @@ def generate_video(experiment_base_folder, camera_id=3, weight_id=5309446, do_ta
             cv2.resize(weight_img, None, rgb_data[-weight_fig_dimensions[0]:, -weight_fig_dimensions[1]:, :], fx=weight_scale, fy=weight_scale)
 
         # Output the image (show it and write to file)
-        cv2.imshow("Frame", rgb_data)
-        video_out.write(rgb_data)
+        if save_video:
+            video_out.write(rgb_data if out_scale==1 else cv2.resize(rgb_data, None, fx=out_scale, fy=out_scale))
         print("{} out of {} frames ({:6.2f}%) written!".format(n+1, len(t_cam), 100.0*(n+1)/len(t_cam)))
 
-        # Let the visualization be stopped by pressing a key
-        k = cv2.waitKey(1)
-        if k > 0:
-            print('Key pressed, exiting!')
-            break
+        if visualize:
+            cv2.imshow("Frame", rgb_data if out_scale==1 else cv2.resize(rgb_data, None, fx=out_scale, fy=out_scale))
+            # Let the visualization be stopped by pressing a key
+            k = cv2.waitKeyEx(1)
+            do_skip_frames = False
+            if k == 63234:  # Left arrow (at least on my Mac)
+                weight_to_cam_t_offset -= LEFT_RIGHT_MULTIPLIER*TIME_INCREMENT
+            elif k == 63235:  # Right arrow
+                weight_to_cam_t_offset += LEFT_RIGHT_MULTIPLIER*TIME_INCREMENT
+            elif k == 63232:  # Up arrow
+                weight_to_cam_t_offset -= TIME_INCREMENT
+            elif k == 63233:  # Down arrow
+                weight_to_cam_t_offset += TIME_INCREMENT
+            elif k == ord('a'):
+                n -= LEFT_RIGHT_MULTIPLIER*FRAME_INCREMENT
+                do_skip_frames = True
+            elif k == ord('d'):
+                n += LEFT_RIGHT_MULTIPLIER*FRAME_INCREMENT
+                do_skip_frames = True
+            elif k == ord('w'):
+                n += FRAME_INCREMENT
+                do_skip_frames = True
+            elif k == ord('s'):
+                n -= FRAME_INCREMENT
+                do_skip_frames = True
+            elif k == ord(' '):
+                is_paused = not is_paused
+            elif k > 0:
+                print('Key pressed, exiting!')
+                break
+
+            if do_skip_frames:
+                video_in.set(cv2.CAP_PROP_POS_FRAMES, n)
+                n = int(video_in.get(cv2.CAP_PROP_POS_FRAMES))  # Don't let it go over the length of the video
 
     # Close video file
-    video_out.release()  # Make sure to release the video so it's actually written to disk
-    print("Video successfully saved as '{}'! :)".format(video_filename))
+    if save_video:
+        video_out.release()  # Make sure to release the video so it's actually written to disk
+        print("Video successfully saved as '{}'! :)".format(video_filename))
 
 
 if __name__ == '__main__':
